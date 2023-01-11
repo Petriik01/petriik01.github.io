c9e813fa4b8b3d8a2952d3d0138e6bb7da8320f9
Petriik01 <81877597+Petriik01@users.noreply.github.com>
2023-01-11 19:43:25 +0100

    gagth.ic

---
+++ gagth.ic/index.php
@@ -0,0 +1,250 @@
+
+
+<!doctype html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Petr Hubert ((Petriik1))">
+    <meta name="description" content="GAG Transportation Holdings je firma pocházející ze státu Red County. Specializujeme se na přepravu balíku, bezpečností služby a kamionovou dopravu.">
+
+    <link rel="stylesheet" href="https://artic.leosight.cz/design/css/bootstrap5.min.css">
+    <link rel="stylesheet" href="https://artic.leosight.cz/design/css/common.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://fonts.googleapis.com/css?family=Cardo:400,700|Oswald" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+
+    <script src="https://artic.leosight.cz/design/js/jquery.min.js"></script>
+    <script src="https://artic.leosight.cz/design/js/bootstrap5.min.js"></script>
+    <script src="https://artic.leosight.cz/design/js/common.js"></script>
+
+    <title>GAG Transportation Holdings</title>
+</head>
+<body>
+<header>
+    <div class="text-dark py-2 shadow">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start mt-2 mb-2 nav-linear">
+                <a href="#" class="d-flex align-items-center my-lg-0 me-lg-auto text-decoration-none">
+                    <img src="images/83.png" width="75" height="75" alt="">
+                    <h4 class="text-black px-2 mb-0">Transportation <span>Holdings</span></h4>
+                </a>
+                <span style="font-size:30px;cursor:pointer;" class="sidenavbtn mx-4 px-4" onclick="openNav()">&#9776;</span>
+
+                <ul class="nav col-12 col-lg-auto justify-content-center text-black my-md-0 fs-5">
+                    <li>
+                        <a href="#" class="nav-link text-black p-0 m-3">
+                            Domů
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#about" class="nav-link text-black p-0 m-3">
+                            O nás
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#vehicles" class="nav-link text-black p-0 m-3">
+                            Vozy
+                        </a>
+                    </li>
+                    <li>
+
+                        <a href="#bonus" class="nav-link text-black p-0 m-3">
+                            Prémie
+                        </a>
+                    </li>
+                    <li>
+
+                        <a href="#contact" class="nav-link text-black p-0 m-3">
+                            Kontakt
+                        </a>
+                    </li>
+                </ul>
+                <div id="mySidenav" class="sidenav">
+                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                    <a href="#">Domů</a>
+                    <a href="#about">O nás</a>
+                    <a href="#vehicles">Vozy</a>
+                    <a href="#bonus">Prémie</a>
+                    <a href="#contact">Kontakt</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="home home-image">
+    <div class="align-items-center g-5 d-flex">
+        <div class="container content mx-4">
+            <h1 class="display-4 text-white"><span>GA</span>G Transporation Holdings</h1>
+            <p class="fs-5 text-white py-2 px-2">Jsme ti, kteří udávají směr logistiky v Red County</p>
+            <a href="https://forms.gle/5woJYzfNLH7Ld3oG9" target="_blank" class="links mx-1">Přidej se k nám</a>
+        </div>
+    </div>
+</section>
+
+<div class="container mt-4 pt-1 mb-4" id="about">
+    <div class="row">
+        <div class="col-md-7">
+            <img src="images/firemni-sidlo.jpg" alt="">
+            <i class="mt-1 px-1">Firemní sídlo</i>
+        </div>
+        <div class="col-md-5">
+            <h3 class="">Kdo jsme?</h3>
+            <p>GAG Transportation Holdings je firma pocházející ze státu Red County. Specializujeme se na přepravu balíku, bezpečností služby, kamionovou dopravu a přepravu ropy. Firma vznikla v roce 2021 a za tu dobu nasbírala hromadu zkušeností na trhu v Red County, v momentální chvíli firma patří mezi 3 nejproduktivnější firmy. V roce 2022 jsme učinili zásadní rozhodnutí a přijali jsme poslání vstoupit mezi ropnou logistiku, tento krok se nám velmi vydařil a v momentální chvíli patříme k průkopníkům v tomto oboru. V momentální chvíli firma GAG Transportation Holdings má více než 70 zaměstnanců a plánuje své řady rozšiřovat. V roce 2021 přišla doba pokroku a firma nakoupila řadu nových tahačů značek Scania, Freightliner a Mack. Došlo ke změnám i v oblasti bezpečnosti, bylo nakoupeno 5 nových osobních vozidel pro bezpečností obor a firemní sídlo dostalo nové kamerové zabezpečení. Firma GAG Transportation Holdings je stále v pohybu a doufáme, že tento vývoj budete sledovat s námi.</p>
+        </div>
+    </div>
+</div>
+<div class="red p-4 text-light" id="vehicles">
+    <div class="container">
+        <div class="mt-4 pb-4">
+            <div class="row">
+                <div class="col-md-6 align-items-center d-flex">
+                    <div class="container">
+                        <h2>GAG Shop</h2>
+                        <p class="mb-1">Nacházím se v <strong>Montgomery</strong> a nabízím Vám širokou nabídku surovin pro výrobu. Lze u mě koupit klíče od beden či samotné bedny.</p>
+                        <p>V případě objednávky kontaktuj prodavače prodejny. Doplnění skladu bývá každé poledne ve všední den.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 text-center">
+                    <img src="images/obchod.jpg" alt="" class="image shadow">
+                </div>
+                <div class="col-md-6 mt-4">
+                    <img src="images/kamiony.jpg" alt="" class="image shadow">
+                </div>
+                <div class="col-md-6 align-items-center d-flex mt-4">
+                    <div class="container">
+                        <h2>Získej vlastní kamion v <span>GA</span>G Holdings!</h2>
+                        <p class="mb-1">Vstupem do naší firmy získáš vlastní kamión, který budeš sdílet se svým kolegou.</p>
+                        <p class="mb-1">Dále nabízíme široký výběr zaměstnání. V případě zručnosti ti nabídneme post elektrikáře a pomůžeš nám opravovat elektrické sítě po selém San Andreas.</p>
+                        <p>Chceš pracovat z pohodlí svého auta? Rozvážej náhradní díly do aut a vydělávej více jak 18 tisíc za zakázku.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="container mt-4" id="bonus">
+        <div class="row">
+            <div class="col-md-6 align-items-center d-flex">
+                <div class="container mt-4 mb-4">
+                    <div class="col-12">
+                        <h2>Prémie </h2>
+                        <p>Našim zaměstnancům pravidelně vyplácíme prémie na základě jejich odvedené práce, na našich zaměstnancích nám velmi záleží a jsme v této sekci dominantní</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 text-center display-1 mobile mt-4 mb-4">
+                <i style="color: #b82207" class="fa-solid fa-money-bill-1-wave"></i>
+            </div>
+        </div>
+        <table class="table tabulka">
+            <tr style="transform: none; cursor: default">
+                <th colspan="8" class="text-center">SYSTÉM PRÉMIÍ</th>
+            </tr>
+            <tr>
+                <td>Týdenní výdělek 250 000 Kč </td>
+                <td> prémie 100 000 Kč</td>
+            </tr>
+            <tr>
+                <td>Týdenní výdělek 500 000 Kč</td>
+                <td>prémie 200 000 Kč</td>
+            </tr>
+            <tr>
+                <td>Týdenní výdělek 750 000 Kč</td>
+                <td>prémie 300 000 Kč</td>
+            </tr>
+            <tr>
+                <td>Týdenní výdělek 1 000 000 Kč</td>
+                <td>prémie 400 000 Kč</td>
+            </tr>
+            <tr>
+                <td>Týdenní výdělek 1 250 000 Kč</td>
+                <td>prémie 500 000 Kč</td>
+            </tr>
+            <tr>
+                <td>Týdenní výdělek 1 500 000 Kč</td>
+                <td>prémie 600 000 Kč</td>
+            </tr>
+            <tr>
+                <td>Týdenní výdělek 1 750 000 Kč</td>
+                <td>prémie 700 000 Kč</td>
+            </tr>
+            <tr>
+                <td>Týdenní výdělek 2 000 000 Kč</td>
+                <td>prémie 800 000 Kč</td>
+            </tr>
+        </table>
+</div>
+<div class="container pb-4">
+    <div class="row">
+        <div class="col-md-6 align-items-center d-flex">
+            <div class="container mt-2">
+                <div class="col-12 content">
+                    <h1>Pracuj u nás!</h1>
+                    <p>Zaujali jsme tě a máš zájem u nás pracovat? Příjď k nám na firmu nebo vyplň krátký formulář a třeba se ozveme právě tobě!</p>
+                    <div class="pt-2">
+                        <a href="https://forms.gle/5woJYzfNLH7Ld3oG9" target="_blank" class="links">Mám zájem</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 text-center mobile">
+            <img src="images/map.jpg" alt="" class="image shadow">
+        </div>
+    </div>
+</div>
+
+
+
+
+<div class="red text-light p-4 mt-4">
+    <div class="container">
+        <h2 class="mb-4" id="contact">Kontakt <i class="fa-solid fa-address-card"></i></h2>
+        <div class="row">
+            <div class="col-md-6">
+                <h5 class="mt-2">Provozovatel</h5>
+                <p class="mb-1">GAG Transportation Holdings</p>
+                <p class="mb-1">Whitewood Estates, Las Venturas</p>
+                <p class="mb-1">IČO: 83</p>
+                <p>Firma je zapsána v restříku firem</p>
+            </div>
+            <div class="col-md-6">
+                <h5 class="mt-2">Kontakt na vedení firmy <i class="fa-solid fa-phone"></i></h5>
+                <p class="mb-1">Gustavo Gaviria - gustavo.gaviria@post.ic - 332 600</p>
+            </div>
+        </div>
+    </div>
+    <footer class="footer mt-4">
+        <div class="text-center">
+            <div class="text-light">
+                <hr>
+                <p class="m-0">GAG Transportation Holdings <?php echo date("Y")?></p>
+                <p class="m-0">Coded by <a href="https://artic.leosight.cz/ergonomy.ic" style="color: #b82207">Ergonomy</a> Team</p>
+            </div>
+        </div>
+    </footer>
+</div>
+
+
+<script>
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+
+    window.onscroll = () =>{
+        document.getElementById("mySidenav").style.width = "0";
+    }
+</script>
+
+</body>
+</html>
+
